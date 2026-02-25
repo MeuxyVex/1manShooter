d9db8cc25d377d6f96fb86a614d05ef1ce6b3cfb
Vex <138825734+MeuxyVex@users.noreply.github.com>
2026-02-25 23:29:36 +0100

    Delete 1manShooter-main directory

--- 1manShooter-main/Content/__init__.py
+++
@@ -1,3 +0,0 @@
-from .game import maingame
-from .playerconf import joueur
-from .projectile import Projectile
--- 1manShooter-main/Content/game.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from Content.playerconf import joueur
-class maingame:
-    def __init__(self):
-        self.Player = joueur()
-        self.press =  {}
